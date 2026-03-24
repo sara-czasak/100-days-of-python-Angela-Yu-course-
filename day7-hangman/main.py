021c41992f047be9116633f33a3132fdb9c52641
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-24 12:56:49 +0100

    adding functionality

--- day7-hangman/main.py
+++ day7-hangman/main.py
@@ -18,15 +18,31 @@ for i in range(len(fraze)):
 # Setting up lives
 lives = 10
 
+# Making empty list for guessed letters:
+guesses = []
+
 # Game loop
 play = True
 
 while play:
     print(blank)
+    print(f"*** LIVES LEFT: {lives} ***")
     guess = input("Guess a letter: ")
-    for index, i in enumerate(fraze):
-        if i.lower() == guess.lower():
-            blank = blank[:index] + guess + blank[index + 1:]
-            if "_" not in blank:
-                print("*** YOU WIN ***")
+
+    if guess.lower() in fraze and guess.lower() not in guesses:
+        guesses.append(guess.lower())
+        for index, i in enumerate(fraze):
+            if i.lower() == guess.lower():
+                blank = blank[:index] + guess + blank[index + 1:]
+                if "_" not in blank:
+                    print("*** YOU WIN ***")
+    elif guess.lower() in guesses:
+        print(f"*** YOU ALREADY GUESSED LETTER: {guess}")
+    else:
+        guesses.append(guess.lower())
+        lives -= 1
+        if lives == 0:
+            print("*** GAME OVER ***")
+            print(f"*** THE FRAZE WAS: {fraze}")
+            play = False
 
