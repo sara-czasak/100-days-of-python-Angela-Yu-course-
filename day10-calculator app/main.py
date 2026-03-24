0e1afcad59f906634398e6594198adfc9aa942ef
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-24 17:26:48 +0100

    bug fix

--- day10-calculator app/main.py
+++ day10-calculator app/main.py
@@ -1,16 +1,25 @@
-def calculator(a, b, operator):
+def calculator(a, operator, b):
     if a == "" or b == "" or operator == "":
         return "I'm sorry, I didn't catch that.."
     match operator:
         case "+":
-            return f"{int(a)} + {int(b)} = {int(a) + int(b)}"
+            return f"{float(a)} + {float(b)} = {float(a) + float(b)}"
         case "-":
-            return f"{int(a)} - {int(b)} = {int(a) - int(b)}"
+            return f"{float(a)} - {float(b)} = {float(a) - float(b)}"
         case "*":
-            return f"{int(a)} * {int(b)} = {int(a) * int(b)}"
+            return f"{float(a)} * {float(b)} = {float(a)* float(b)}"
         case "/":
-            return f"{int(a)} / {int(b)} = {int(a) / int(b)}"
+            return f"{float(a)} / {float(b)} = {float(a) / float(b)}"
 
 print("*** WELCOME TO THE CALCULATOR ***")
-print(calculator(a=input("Enter first number"), b=input("Enter the second number: "), operator=input("Enter the operator (+, -, *, /): "),
-))
+
+
+calculate = True
+
+while calculate:
+    print(calculator(a=input("Enter first number: "), operator=input("Enter the operator (+, -, *, /): "), b=input("Enter the second number: ")))
+    again = input("Would you like to calculate again? (y/n): ").lower()
+    if again == "y":
+        calculate = True
+    else:
+        calculate = False
