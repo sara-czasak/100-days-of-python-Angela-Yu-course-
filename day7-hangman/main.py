85aff9deddd821bd10afb43309541766f820ee07
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-24 12:27:14 +0100

    small refactor

--- day7-hangman/main.py
+++ day7-hangman/main.py
@@ -1,29 +1,3 @@
-print("*** WELCOME TO HANGMAN ***")
+from words_to_guess import *
 
-words = [
-"Down by the water",
-"Tell the truth",
-'Around the clock',
-"Think before you act",
-"Any old time",
-"It’s a small world",
-"Read between the lines",
-"Always be kind",
-"In the big city",
-"Leave it to me",
-"First day of school",
-"Ever since I knew you",
-"It happened today",
-"We need more space",
-"What happened here?",
-"Twenty-four hours a day",
-"Against all odds",
-"Tried and true",
-"Go figure",
-"Today was better",
-"Who brought the cake?",
-'My circle of friends',
-"Twelve inches to a foot",
-"What’s the difference?",
-"I believe in you"
-]
+print("*** WELCOME TO HANGMAN ***")
