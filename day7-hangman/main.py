ac23930525e55f9b2f8d25fc3a3b133b2bf7fac2
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-24 13:09:46 +0100

    add play again functionality

--- day7-hangman/main.py
+++ day7-hangman/main.py
@@ -3,46 +3,59 @@ from words_to_guess import *
 
 print("*** WELCOME TO HANGMAN ***")
 
-# Generating random fraze for player to guess
-fraze = random.choice(frazes)
-# blanks screen
-blank = ""
 
-# Generating string with _ for each letter of chosen random fraze
-for i in range(len(fraze)):
-    if fraze[i] == " ":
-        blank += " "
-    else:
-        blank += "_"
-
-# Setting up lives
-lives = 10
-
-# Making empty list for guessed letters:
-guesses = []
 
 # Game loop
+again = True
 play = True
 
-while play:
-    print(blank)
-    print(f"*** LIVES LEFT: {lives} ***")
-    guess = input("Guess a letter: ")
+while again:
 
-    if guess.lower() in fraze and guess.lower() not in guesses:
-        guesses.append(guess.lower())
-        for index, i in enumerate(fraze):
-            if i.lower() == guess.lower():
-                blank = blank[:index] + guess + blank[index + 1:]
-                if "_" not in blank:
-                    print("*** YOU WIN ***")
-    elif guess.lower() in guesses:
-        print(f"*** YOU ALREADY GUESSED LETTER: {guess}")
+    # Generating random fraze for player to guess
+    fraze = random.choice(frazes)
+    # blanks screen
+    blank = ""
+
+    # Generating string with _ for each letter of chosen random fraze
+    for i in range(len(fraze)):
+        if fraze[i] == " ":
+            blank += " "
+        else:
+            blank += "_"
+
+    # Setting up lives
+    lives = 10
+
+    # Making empty list for guessed letters:
+    guesses = []
+
+    while play:
+        print(blank)
+        print(f"*** LIVES LEFT: {lives} ***")
+        guess = input("Guess a letter: ")
+
+        if guess.lower() in fraze and guess.lower() not in guesses:
+            guesses.append(guess.lower())
+            for index, i in enumerate(fraze):
+                if i.lower() == guess.lower():
+                    blank = blank[:index] + guess + blank[index + 1:]
+                    if "_" not in blank:
+                        print("*** YOU WIN ***")
+        elif guess.lower() in guesses:
+            print(f"*** YOU ALREADY GUESSED LETTER: {guess}")
+        else:
+            guesses.append(guess.lower())
+            lives -= 1
+            if lives == 0:
+                print("*** GAME OVER ***")
+                print(f"*** THE FRAZE WAS: {fraze}")
+                play = False
+    play_again = input("Would you like to play again? (y/n):\n").lower()
+    if play_again == "y":
+        again = True
+        play = True
     else:
-        guesses.append(guess.lower())
-        lives -= 1
-        if lives == 0:
-            print("*** GAME OVER ***")
-            print(f"*** THE FRAZE WAS: {fraze}")
-            play = False
+        print("Thank you for playing!")
+        again = False
 
+print("*** SEE YOU NEXT TIME ***")
