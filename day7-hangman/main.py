8d07ba598800ffee4e7ccffc3a8fa77df8cf23bb
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-24 12:47:33 +0100

    add functionality

--- day7-hangman/main.py
+++ day7-hangman/main.py
@@ -1,3 +1,32 @@
+import random
 from words_to_guess import *
 
 print("*** WELCOME TO HANGMAN ***")
+
+# Generating random fraze for player to guess
+fraze = random.choice(frazes)
+# blanks screen
+blank = ""
+
+# Generating string with _ for each letter of chosen random fraze
+for i in range(len(fraze)):
+    if fraze[i] == " ":
+        blank += " "
+    else:
+        blank += "_"
+
+# Setting up lives
+lives = 10
+
+# Game loop
+play = True
+
+while play:
+    print(blank)
+    guess = input("Guess a letter: ")
+    for index, i in enumerate(fraze):
+        if i.lower() == guess.lower():
+            blank = blank[:index] + guess + blank[index + 1:]
+            if "_" not in blank:
+                print("*** YOU WIN ***")
+
--- day7-hangman/words_to_guess.py
+++ day7-hangman/words_to_guess.py
@@ -1,10 +1,9 @@
-words = [
+frazes = [
 "Down by the water",
 "Tell the truth",
 'Around the clock',
 "Think before you act",
 "Any old time",
-"It’s a small world",
 "Read between the lines",
 "Always be kind",
 "In the big city",
@@ -14,14 +13,12 @@ words = [
 "It happened today",
 "We need more space",
 "What happened here?",
-"Twenty-four hours a day",
+"Twenty four hours a day",
 "Against all odds",
 "Tried and true",
 "Go figure",
 "Today was better",
 "Who brought the cake?",
-'My circle of friends',
 "Twelve inches to a foot",
-"What’s the difference?",
 "I believe in you"
 ]
