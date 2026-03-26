ceb4be6175289d5f16e6b368e6e0e504010e744a
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-26 09:01:01 +0100

    add data for game

---
+++ day14-higher lower game/game_data.py
@@ -0,0 +1,62 @@
+data = [
+    {
+        "author": "Leo Tolstoy",
+        "word_count": 587287,
+        "book_title": "War and Peace",
+        "country": "Russia"
+    },
+    {
+        "author": "Miguel de Cervantes",
+        "word_count": 430269,
+        "book_title": "Don Quixote",
+        "country": "Spain"
+    },
+    {
+        "author": "Alexandre Dumas",
+        "word_count": 464162,
+        "book_title": "The Count of Monte Cristo",
+        "country": "France"
+    },
+    {
+        "author": "Victor Hugo",
+        "word_count": 568751,
+        "book_title": "Les Miserables",
+        "country": "France"
+    },
+    {
+        "author": "J. R. R. Tolkien",
+        "word_count": 564187,
+        "book_title": "The Lord of the Rings",
+        "country": "Britain"
+    },
+    {
+        "author": "Homer",
+        "word_count":193536,
+        "book_title": "The Iliad",
+        "country": "Greece"
+    },
+    {
+        "author": "Bram Stoker",
+        "word_count":165453,
+        "book_title": "Dracula ",
+        "country": "Ireland"
+    },
+    {
+        "author": "Charles Dickens",
+        "word_count": 161712,
+        "book_title": "Oliver Twist",
+        "country": "Britain"
+    },
+    {
+        "author": "Jane Austen",
+        "word_count": 124713,
+        "book_title": "Pride and Prejudice",
+        "country": "Britain"
+    },
+    {
+        "author": "Mark Twain",
+        "word_count": 118804,
+        "book_title": "Adventures of Huckleberry Finn",
+        "country": "United States"
+    },
+]
