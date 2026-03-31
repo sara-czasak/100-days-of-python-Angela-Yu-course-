8b1bfcdc921690c061736c2a2d5d88c24a971e35
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-31 14:49:48 +0200

    dict comprehension

--- day26-NATO alphabet/main.py
+++ day26-NATO alphabet/main.py
@@ -1,8 +1,17 @@
-
 import pandas as pd
+import random
+
 
 data = pd.read_csv('nato_phonetic_alphabet.csv')
-letters = data['letter'].to_list()
-codes = data['code'].to_list()
 
-# to_nato_alphabet = input('Enter word you: ')
+print("\n*** WELCOME TO NATO ALPHABET HELPER ***")
+print("-" * 40)
+
+word = input('Enter a word: ').upper()
+
+nato_alphabet = {row.letter:row.code for (index, row) in data.iterrows()}
+
+nato_word = [nato_alphabet[letter] for letter in word]
+
+print('Word in nato phonetic alphabet:\n', nato_word)
+print("-" * 40)
