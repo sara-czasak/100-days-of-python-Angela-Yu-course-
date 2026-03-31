9bb9a4887d901763eaa1facb412a26f881315ee2
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-31 19:22:37 +0200

    converter

--- day27-Unit Converter/main.py
+++ day27-Unit Converter/main.py
@@ -1,11 +1,31 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+
+def convert():
+    kilometers = int(miles.get()) * 1.6
+    km['text'] = str(kilometers)
+
+
+window = Tk()
 window.title('Km to Mile Converter')
-window.minsize(500, 300)
+window.minsize(200, 100)
 
-label = tk.Label(window, text="I'm a label", fg="red", font=("Arial", 20, 'bold'))
-label.pack()
+miles = Entry(window)
+miles.grid(row=0, column=1, padx=10, pady=10)
 
+miles_label = Label(window, text="Miles")
+miles_label.grid(row=0, column=2, padx=10, pady=10)
+
+equal_label = Label(window, text="is equal to")
+equal_label.grid(row=1, column=0, padx=10)
+
+km = Label(window, text="0")
+km.grid(row=1, column=1)
+
+km_label = Label(window, text="Km")
+km_label.grid(row=1, column=2)
+
+button = Button(window, text="Convert", command=convert)
+button.grid(row=2, column=1)
 
 window.mainloop()
