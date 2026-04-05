80dfc4cad2b3350291cdf824964a2121a2d5b285
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-05 20:49:30 +0200

    ISS overhead project complete

--- day33-ISS overhead notifier/main.py
+++ day33-ISS overhead notifier/main.py
@@ -1,5 +1,15 @@
 import requests
 import datetime as dt
+import smtplib
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# CONSTANTS
+my_email = os.getenv('my_email')
+app_password = os.getenv('app_password')
 
 
 # Getting sunrise/sunset data
@@ -10,13 +20,15 @@ parameters = {
     'formatted': 0,
 }
 
+my_position = (parameters['lat'], parameters['lng'])
+
 time_now = dt.datetime.now().strftime("%H")
 
 response = requests.get(f'https://api.sunrise-sunset.org/json', params=parameters)
 response.raise_for_status()
 
-sunrise = response.json()['results']['sunrise'][11:13]
-sunset = response.json()['results']['sunset'][11:13]
+sunrise = int(response.json()['results']['sunrise'][11:13])
+sunset = int(response.json()['results']['sunset'][11:13])
 
 
 # Getting ISS data
@@ -24,6 +36,32 @@ response = requests.get('http://api.open-notify.org/iss-now.json')
 response.raise_for_status()
 
 data = response.json()
-latitude = data['iss_position']['latitude']
-longitude = data['iss_position']['longitude']
+latitude = float(data['iss_position']['latitude'])
+longitude = float(data['iss_position']['longitude'])
 iss_position = (latitude, longitude)
+
+
+def check_if_within_range(iss_pos):
+    if 55 >= iss_pos[0] <= 65 and 14 >= iss_pos[1] <= 19:
+        return True
+    else:
+        return False
+
+
+def check_if_dark(time, sunrise, sunset):
+    if time < sunrise and time > sunset:
+        return True
+    else:
+        return False
+
+
+if check_if_within_range(iss_position) and check_if_dark(time_now, sunrise, sunset):
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=app_password)
+        connection.sendmail(from_addr=my_email, to_addrs='sara.p.czasak.m@gmail.com', msg='Subject:ISS is overhead!\n\nLook up! You can see the ISS in the sky now!')
+else:
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=app_password)
+        connection.sendmail(from_addr=my_email, to_addrs='sara.p.czasak.m@gmail.com', msg='Subject:ISS is not overhead!\n\nSorry, the ISS is not around right now..')
