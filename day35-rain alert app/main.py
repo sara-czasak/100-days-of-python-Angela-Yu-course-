918d8ee207d8f4f41807dfa464783b45e07277b3
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-06 22:30:40 +0200

    getting weather data

--- day35-rain alert app/main.py
+++ day35-rain alert app/main.py
@@ -1,1 +1,19 @@
 import requests
+import os
+import dotenv
+
+dotenv.load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+
+parameters = {
+    "q": "Krakow",
+    "appid": API_KEY
+}
+
+
+response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=parameters)
+
+
+weather = response.json()['weather'][0]['main']
+
