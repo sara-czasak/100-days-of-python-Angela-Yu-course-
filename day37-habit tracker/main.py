7846010c8732f8012c858c09064fb372086d859f
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-10 15:07:21 +0200

    making new graph with post request

--- day37-habit tracker/main.py
+++ day37-habit tracker/main.py
@@ -6,16 +6,34 @@ import dotenv
 dotenv.load_dotenv()
 
 PIXELA_AUTH = os.getenv("PIXELA_AUTH")
+PIXELA_USERNAME = os.getenv("PIXELA_USERNAME")
 PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
 
 
 # REMEMBER username must be lowercase!
 pixela_params = {
     'token': PIXELA_AUTH,
-    'username':'starwasp7272',
+    'username': PIXELA_USERNAME,
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes'
 }
 
-response = requests.post(PIXELA_ENDPOINT, json=pixela_params)
-print(response.json())
+# response = requests.post(PIXELA_ENDPOINT, json=pixela_params)
+# print(response.json())
+
+GRAPH_ENDPOINT = f'{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs'
+
+graph_config = {
+    'id': 'graph1',
+    'name': 'Beading Tracker',
+    'unit': 'item',
+    'type': 'int',
+    'color': 'ajisai'
+}
+
+request_header = {
+    'X-USER-TOKEN': PIXELA_AUTH,
+}
+
+response = requests.post(GRAPH_ENDPOINT, json=graph_config, headers=request_header)
+print(response.text)
