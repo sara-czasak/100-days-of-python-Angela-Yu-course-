6eb2cd297f4c7cb0b01e4bfd5410aa871a95a5c1
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-10 14:55:04 +0200

    make post request

--- day37-habit tracker/main.py
+++ day37-habit tracker/main.py
@@ -8,11 +8,14 @@ dotenv.load_dotenv()
 PIXELA_AUTH = os.getenv("PIXELA_AUTH")
 PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
 
+
+# REMEMBER username must be lowercase!
 pixela_params = {
     'token': PIXELA_AUTH,
-    'username':'StarWasp',
+    'username':'starwasp7272',
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes'
 }
 
-response = requests.post(PIXELA_ENDPOINT, params=pixela_params)
+response = requests.post(PIXELA_ENDPOINT, json=pixela_params)
+print(response.json())
