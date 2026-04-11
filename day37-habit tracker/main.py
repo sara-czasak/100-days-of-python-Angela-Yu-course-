7c0eaf527af31e2503eb2282bd8cd0479241f0ce
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-11 18:11:59 +0200

    add post request to post pixel

--- day37-habit tracker/main.py
+++ day37-habit tracker/main.py
@@ -1,6 +1,12 @@
 import requests
 import os
 import dotenv
+import datetime as dt
+
+
+today = (dt.date.today()).strftime("%Y%m%d")
+print(today)
+
 
 
 dotenv.load_dotenv()
@@ -8,7 +14,7 @@ dotenv.load_dotenv()
 PIXELA_AUTH = os.getenv("PIXELA_AUTH")
 PIXELA_USERNAME = os.getenv("PIXELA_USERNAME")
 PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
-
+GRAPH_ID = 'graph1'
 
 # REMEMBER username must be lowercase!
 pixela_params = {
@@ -21,19 +27,31 @@ pixela_params = {
 # response = requests.post(PIXELA_ENDPOINT, json=pixela_params)
 # print(response.json())
 
-GRAPH_ENDPOINT = f'{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs'
-
-graph_config = {
-    'id': 'graph1',
-    'name': 'Beading Tracker',
-    'unit': 'item',
-    'type': 'int',
-    'color': 'ajisai'
-}
-
+# GRAPH_ENDPOINT = f'{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs'
+#
+# graph_config = {
+#     'id': 'graph1',
+#     'name': 'Beading Tracker',
+#     'unit': 'item',
+#     'type': 'int',
+#     'color': 'ajisai'
+# }
+#
 request_header = {
     'X-USER-TOKEN': PIXELA_AUTH,
 }
+#
+# response = requests.post(GRAPH_ENDPOINT, json=graph_config, headers=request_header)
+# print(response.text)
 
-response = requests.post(GRAPH_ENDPOINT, json=graph_config, headers=request_header)
+# Posting a pixel to graph
+
+pixel_config = {
+    'date': today,
+    'quantity': "5"
+}
+
+PIXEL_ENDPOINT = f'{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs/{GRAPH_ID}'
+
+response = requests.post(PIXEL_ENDPOINT, headers=request_header, json=pixel_config)
 print(response.text)
