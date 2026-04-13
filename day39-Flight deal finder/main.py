7f2fea8866cb3307b62c0e8085d12d493789e087
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-13 15:07:24 +0200

    starting project:

---
+++ day39-Flight deal finder/main.py
@@ -0,0 +1,11 @@
+from dataclasses import *
+from flight_search import *
+from flight_data import *
+from notification_manager import *
+import requests
+import os
+import dotenv
+
+
+dotenv.load_dotenv()
+
