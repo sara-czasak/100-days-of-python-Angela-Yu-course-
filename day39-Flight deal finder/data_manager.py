3d3eab7826f6b08c9c4abe6751eecfdcead4829e
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-13 15:39:06 +0200

    get data from sheety

--- day39-Flight deal finder/data_manager.py
+++ day39-Flight deal finder/data_manager.py
@@ -1,3 +1,23 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+# CONSTANTS
+SHEETY_GET_ENDPOINT = os.getenv("SHEETY_GET_ENDPOINT")
+SHEETY_TOKEN = os.getenv("SHEETY_TOKEN")
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.token = SHEETY_TOKEN
+        self.destination_data ={}
+
+    def get_destination_data(self):
+        response = requests.get(SHEETY_GET_ENDPOINT, headers={"Authorization": f"Bearer {self.token}"})
+        data = response.json()
+        self.destination_data = data['prices']
+        return self.destination_data
--- day39-Flight deal finder/main.py
+++ day39-Flight deal finder/main.py
@@ -2,10 +2,31 @@ from dataclasses import *
 from flight_search import *
 from flight_data import *
 from notification_manager import *
+from data_manager import *
 import requests
 import os
 import dotenv
+from pprint import pprint
+import requests_cache
+from datetime import datetime, timedelta
 
 
+requests_cache.install_cache()
 dotenv.load_dotenv()
 
+
+# CONSTANTS
+TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+GOOGLE_FLIGHT_API_ENDPOINT = os.getenv("GOOGLE_FLIGHT_API_ENDPOINT")
+
+SHEETY_POST_ENDPOINT = os.getenv("SHEETY_POST_ENDPOINT")
+SHEETY_PUT_ENDPOINT = os.getenv("SHEETY_PUT_ENDPOINT")
+tomorrow = datetime.today() + timedelta(days=1)
+six_moths_from_now = datetime.today() + timedelta(days=182)
+
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+pprint(sheet_data)
