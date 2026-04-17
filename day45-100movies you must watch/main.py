77411074c914455614eae8e3c82fb52593799246
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-17 19:35:00 +0200

    finding elements with bs4

--- day45-100movies you must watch/main.py
+++ day45-100movies you must watch/main.py
@@ -10,3 +10,28 @@ soup = BeautifulSoup(content, 'html.parser')
 
 # .string gets the content of the tag
 title = soup.title.string
+
+# find all tags
+a = soup.find_all('a')
+p = soup.find_all('p')
+
+# for tag in a:
+#     # get value of any of the attributes, in this case href
+#     print(tag.get('href'))
+
+
+# get tag by id
+# heading = soup.find('h1', id='name')
+
+
+# get tag by class (class is class_ not to clash with object class creation
+# section_heading = soup.find('h3', class_='heading')
+#
+#
+# company_url = soup.select_one(selector='p a')
+#
+# name = soup.select_one(selector='#name')
+#
+# heading1 = soup.select('.heading')
+#
+# print(heading1)
