d55b03432cfa168b9b917a971d7f2f10e5d54dd8
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-18 14:07:09 +0200

    finished project for day 45

--- day45-100movies you must watch/main.py
+++ day45-100movies you must watch/main.py
@@ -3,6 +3,12 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
 
+# !!! Use .content not .text, .text sometimes decodes the html badly leaving some html encoding in !!!
+r = requests.get(URL).content
+soup = BeautifulSoup(r, 'html.parser')
 
+titles = soup.find_all(name='h3', class_='title')
+with open('movies.txt', 'w', encoding='UTF-8') as f:
+    for title in titles[::-1]:
+        f.write(f'{title.text}\n')
