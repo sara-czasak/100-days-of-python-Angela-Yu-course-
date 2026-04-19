6da9e39961b83d213db81b8e8eb0d53f261d3b76
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-19 17:31:18 +0200

    practice3 complete

--- day48-cookie clicker/practice/practice3.py
+++ day48-cookie clicker/practice/practice3.py
@@ -11,6 +11,9 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get(url='https://en.wikipedia.org/wiki/Main_Page')
 
-
+# Get number of articles in english
+# articles_in_english = driver.find_element(By.XPATH, '//*[@id="articlecount"]/ul/li[2]/a[1]').text
+articles_in_english = driver.find_elements(By.CSS_SELECTOR, '#articlecount a')[1]
+print(articles_in_english.text)
 
 driver.quit()
