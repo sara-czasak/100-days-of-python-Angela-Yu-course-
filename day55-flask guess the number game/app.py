7b8cd6df94fd5e2a03c4b2e2c491fc07d0ca77e5
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-04-20 11:15:40 +0200

    add hidden path with joke

--- day55-flask guess the number game/app.py
+++ day55-flask guess the number game/app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/secret')
+def secret():
+    return '<h1>What do you call a well-balanced horse?\n<em>STABLE<em></h1>'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
